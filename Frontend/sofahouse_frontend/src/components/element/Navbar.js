492dd1f4b4400eea7956179debf6f70d1a47c49f
Piriya Chaigul <pryeditor@gmail.com>
2022-02-08 00:52:37 +0700

    Add PlayConsole function

--- Frontend/sofahouse_frontend/src/components/element/Navbar.js
+++ Frontend/sofahouse_frontend/src/components/element/Navbar.js
@@ -4,19 +4,45 @@ import "../../assets/css/components/navbar.css";
 import navLogo from "../../assets/images/nav-logo.png";
 
 export default function Navbar(props) {
-
-
   return (
     <div id="navbar" className="section">
       <div className="navbar-container">
         <div>
           <img id="nav-logo" src={navLogo} alt="" />
         </div>
-        <div id="nav-menu" class="section">
-          <h1 onClick={() => linkPath('home')} className={`bg-text ${ props.path === "home" ? "bg-text nav-active" : null}`}>Home</h1>
-          <h1 onClick={() => linkPath('portfolio')} className={`bg-text ${ props.path === "portfolio" ? "bg-text nav-active" : null}`}>Portfolio</h1>
-          <h1 onClick={() => linkPath('service')} className={`bg-text ${ props.path === "service" ? "bg-text nav-active" : null}`}>Service</h1>
-          <h1 onClick={() => linkPath('aboutus')} className={`bg-text ${ props.path === "aboutus" ? "bg-text nav-active" : null}`}>About Us</h1>
+        <div id="nav-menu" className="section">
+          <h1
+            onClick={() => linkPath("")}
+            className={`bg-text ${
+              props.path === "/" ? "bg-text nav-active" : null
+            }`}
+          >
+            Home
+          </h1>
+          <h1
+            onClick={() => linkPath("portfolio")}
+            className={`bg-text ${
+              props.path === "portfolio" ? "bg-text nav-active" : null
+            }`}
+          >
+            Portfolio
+          </h1>
+          <h1
+            onClick={() => linkPath("service")}
+            className={`bg-text ${
+              props.path === "service" ? "bg-text nav-active" : null
+            }`}
+          >
+            Service
+          </h1>
+          <h1
+            onClick={() => linkPath("aboutus")}
+            className={`bg-text ${
+              props.path === "aboutus" ? "bg-text nav-active" : null
+            }`}
+          >
+            About Us
+          </h1>
         </div>
       </div>
     </div>
@@ -26,4 +52,4 @@ export default function Navbar(props) {
 let linkPath = (value) => {
   let web = "http://localhost:3000/";
   window.location = web + value;
-}
+};
