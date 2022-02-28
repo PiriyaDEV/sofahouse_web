357fa59ce644e963ebe709e5545b0248e27fc7f6
Piriya Chaigul <pryeditor@gmail.com>
2022-02-28 20:24:14 +0700

    Before Changed

--- Frontend/sofahouse_frontend/src/components/element/Hamburger.js
+++ Frontend/sofahouse_frontend/src/components/element/Hamburger.js
@@ -13,25 +13,25 @@ export default function Hamburger(props) {
       <div className= {`menu-item hamburger-text ${
       props.path === "/" ? "hamburger-active" : null
     }`} onClick={() => linkPath("/")}>
-        <h1 className="bg-text">Home</h1>
+        <h1 className="sm-text">Home</h1>
       </div>
 
       <div className= {`menu-item hamburger-text ${
       props.path === "portfolio" ? "hamburger-active" : null
     }`} onClick={() => linkPath("/portfolio")}>
-        <h1 className="bg-text">Portfolio</h1>
+        <h1 className="sm-text">Portfolio</h1>
       </div>
 
       <div className={`menu-item hamburger-text ${
       props.path === "service" ? "hamburger-active" : null
     }`} onClick={() => linkPath("/service")}>
-        <h1 className="bg-text">Service</h1>
+        <h1 className="sm-text">Service</h1>
       </div>
       
       <div className={`menu-item hamburger-text hamburger-last ${
-      props.path === "about" ? "hamburger-active" : null
+      props.path === "aboutus" ? "hamburger-active" : null
     }`} onClick={() => linkPath("/about")}>
-        <h1 className="bg-text">About Us</h1>
+        <h1 className="sm-text">About Us</h1>
       </div>
     </Menu>
   );
